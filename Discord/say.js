fadbb579c8e71e2a66cd6a882f43ecd1e121469b
SoyCamilo14 <effdyjjd@gmail.com>
2021-05-18 20:44:13 +0200

    ad

---
+++ Discord/say.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { Client, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "say",
+    alias: ["decir"],
+
+run (client, message, args){
+
+  message.delete()
+
+ let texto = args.join(' ')
+ if(!texto) return message.channel.send("Debes escribir algo para que pueda repetirlo!")
+
+ message.channel.send(texto)
+
+    
+ }
+ 
+}
